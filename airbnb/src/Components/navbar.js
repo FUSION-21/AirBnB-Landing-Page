0b23a734b4b2e3e216b46cf3c30914313b4ecb18
FUSIoN <ashutosh.divyanshu@gmail.com>
2022-08-20 00:49:17 +0530

    navbar

--- airbnb/src/Components/navbar.js
+++ airbnb/src/Components/navbar.js
@@ -4,6 +4,10 @@ export default function Navbar(){
     return(
         <nav>
             <img src='./image/logo.png' alt='Logo' className='nav-logo'></img>
+            <div className='info'>
+                <button><pre>Anywhere       Anyweek     <img src='./image/search.png' alt='search logo' width={30}></img></pre></button>
+                
+            </div>
         </nav>
     )
 }
