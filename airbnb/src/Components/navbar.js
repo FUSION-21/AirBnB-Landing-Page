7c57eb3430df09d51726571d5747ca9f097aaaa1
FUSIoN <ashutosh.divyanshu@gmail.com>
2022-08-20 04:47:45 +0530

    added navbar

--- airbnb/src/Components/navbar.js
+++ airbnb/src/Components/navbar.js
@@ -4,10 +4,14 @@ export default function Navbar(){
     return(
         <nav>
             <img src='./image/logo.png' alt='Logo' className='nav-logo'></img>
-            <div className='info'>
-                <button><pre>Anywhere       Anyweek     <img src='./image/search.png' alt='search logo' width={30}></img></pre></button>
-                
-            </div>
+            
+                <button><span>Anywhere</span>    <span>Anyweek</span>   <span>Add Guest</span> <img src='./image/search.png' alt='logo-search' width={30}></img></button>
+
+                <div className='nav-right'>
+                    <span>Become a Host</span>
+                    <span><img src='./image/globe.png' alt='globe' width={15}></img></span>
+                    <span><button className='but'><span><img src='./image/menu.png' alt='menu' width={20}></img></span>   <span><img src='./image/user.png' alt='profile' width={30}></img></span></button></span>
+                </div>
         </nav>
     )
 }
