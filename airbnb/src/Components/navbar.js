12a46083b54281fb77cf5b1f247aeb2ac421b477
FUSIoN <ashutosh.divyanshu@gmail.com>
2022-08-19 22:29:06 +0530

    initial setup

---
+++ airbnb/src/Components/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src='./image/logo.png' alt='Logo' className='nav-logo'></img>
+        </nav>
+    )
+}
